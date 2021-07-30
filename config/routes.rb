a7ae2d5cad65f861c5e82b4e206ca311afb3fab3
pablo martinez <pablomartinezsepulveda@hotmail.com>
2021-07-30 17:39:08 -0400

    se agregó controlador de restaurant´s

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'restaurants/Reviews'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: ["show", "new", "create", "index"] do
     resources :reviews, only: %i[new create]
