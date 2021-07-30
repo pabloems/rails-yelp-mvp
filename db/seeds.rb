43b88911c842ed8bcf5cb9db0439ac724094057b
pablo martinez <pablomartinezsepulveda@hotmail.com>
2021-07-30 17:06:36 -0400

    se agregaron seed´s, 5 restaurant´s

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Comienzo del seed´s"
+Restaurant.destroy_all
+
+puts "Agregando 5 restaurants"
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+  puts "Creamos el restaurant con id #{restaurant.id}"
+end
